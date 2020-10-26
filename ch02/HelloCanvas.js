82a490221fc07c3b37126cc1deb9c219c36eeeec
caesarii <iwangqinghe@gmail.com>
2020-10-26 10:07:26 +0800

    feat: lesson 01 更新

--- ch02/HelloCanvas.js
+++ ch02/HelloCanvas.js
@@ -1,28 +1,23 @@
 // HelloCanvas.js (c) 2012 matsuda
 
 function main() {
-  // Retrieve <canvas> element
   var canvas = document.getElementById('webgl');
 
-  // Get the rendering context for WebGL
+  // 获取 webgl context
   var gl = canvas.getContext('webgl');
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
   }
 
-  // Set clear color
+  // webgl/opengl 基本api使用模式: 声明数据, 初始化数据, 申请内存, 数据写入指定内存, 使用内存中的数据
   // 指定背景颜色: 一次指定, 一直存在
   // 色值: rgba
   gl.clearColor(1.0, 0.0, 0.0, 0.5);
-
-  // opengl
+  // opengl 中对应的api 
   // glClearColor
 
-  // Clear <canvas>
   // 使用指定颜色擦除已绘制的内容
-  // 参数: 颜色缓冲区, 内存区域
+  // 参数: 颜色缓冲区, 特定的内存区域
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // webgl/opengl 基本api使用模式: 声明数据, 初始化数据, 申请内存, 数据写入指定内存, 使用内存中的数据
 }
